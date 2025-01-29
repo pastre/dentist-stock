35c40e49d46f9962fc4b0fe0286cb02ff7be15ae
Bruno Pastre <bruno.pastre@onefootball.com>
2025-01-29 19:20:50 +0100

    refactor: separates DSL and unit test into a separate folder

--- test/dsl/dsl.dart
+++ test/dsl/dsl.dart
@@ -65,33 +65,3 @@ class DentistInventoryTester {
     expect(find.byType(AlertDialog), findsNothing);
   }
 }
-
-void main() {
-  testWidgets('''
-  GIVEN app is open
-  WHEN taps join button
-  THEN join inventory alert must appear
-  BUT join button is disabled
-  ''', (WidgetTester t) async {
-    DentistInventoryTester tester = DentistInventoryTester.from(t);
-    await tester.openApp();
-    await tester.tapJoin();
-    await tester.findsAlert(title: 'Entrar em um estoque');
-    await tester.findsDisabledJoinButton();
-  });
-
-  testWidgets('''
-  GIVEN join inventory alert is open
-  AND an inventory name is typed
-  AND the inventory exists
-  WHEN taps join button
-  THEN join inventory alert must disappear
-  AND the inventory should be displayed
-  ''', (WidgetTester t) async {
-    DentistInventoryTester tester = DentistInventoryTester.from(t);
-    await tester.openApp();
-    await tester.joinInventory(inventoryName: 'Stub inventory');
-    await tester.verifyJoinInventoryClosed();
-    await tester.verifyJoinedInventory(inventoryName: 'Stub inventory');
-  });
-}
